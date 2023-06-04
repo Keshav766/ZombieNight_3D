f4ced0ec4abd1d7cbd201a41af9cf7dbc6f53ffa
Keshav Gaur <75837310+Keshav766@users.noreply.github.com>
2023-06-04 20:36:51 +0530

    Initial Commit

---
+++ 9_ZombieNight(3D)/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour
+{
+
+    [SerializeField] Transform playerRef;
+    [SerializeField] float playerDetectionRangeLimit = 20f;
+    [SerializeField] float attackRange = 5f;
+    [SerializeField] Color gismosColor;
+
+    NavMeshAgent enemyNavMeshRef;
+    float distanceToPlayer;
+    bool playerDetected = false;
+    bool playerInAttckRange = false;
+
+    void Start()
+    {
+        enemyNavMeshRef = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        CheckPlayerDistance();
+    }
+
+    private void CheckPlayerDistance()
+    {
+        distanceToPlayer = Vector3.Distance(playerRef.position, transform.position);
+
+        if (distanceToPlayer <= playerDetectionRangeLimit)
+        {
+            EngagePlayer();
+        }
+        
+    }
+
+    void EngagePlayer()
+    {
+        if (distanceToPlayer >= enemyNavMeshRef.stoppingDistance)
+        {
+            ChasePlayer();
+        }
+        if(distanceToPlayer <= enemyNavMeshRef.stoppingDistance)
+        {
+            AttckPlayer();
+        }
+        
+    }
+
+    private void ChasePlayer()
+    {
+        enemyNavMeshRef.SetDestination(playerRef.position);
+    }
+
+    void AttckPlayer()
+    {
+        Debug.Log("I will attack bitch");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = gismosColor;
+        Gizmos.DrawWireSphere(transform.position, playerDetectionRangeLimit);
+    }
+}
